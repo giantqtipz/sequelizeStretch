a8b643fecfc5f76ee4950167988033b84251d3f6
Bima Saridjo <palessie27@gmail.com>
2020-06-22 18:55:53 -0400

    push workshop

--- recipe.js
+++ recipe.js
@@ -1,7 +1,27 @@
 const db = require('./dbConfig');
 const Sequelize = require('sequelize');
+const { STRING, BOOLEAN, INTEGER, ENUM } = require('sequelize');
 
 //YOUR CODE GOES HERE
+const Recipe = db.define('recipe', {
+    name: {
+        type: STRING,
+        defaultValue: 'cereal',
+        allowNull: false
+    },
+    cookTime: {
+        type: INTEGER,
+        min: 1,
+        max: 60
+    },
+    vegan: {
+        type: BOOLEAN
+    },
+    food: {
+        type: ENUM
+    }
+})
+
 
 //--------------------
 module.exports = Recipe;
